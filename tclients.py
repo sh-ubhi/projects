236a8f6515357889236b19c481c4bc7f3fe3b70e
sh-ubhi <143268160+sh-ubhi@users.noreply.github.com>
2024-06-29 12:02:31 +0530

    Add files via upload

--- tclients.py
+++ tclients.py
@@ -101,6 +101,31 @@ def placeNumbers():
     global ticket_grid
     global current_number_list
 
+    # Here key is index and values are numbers 
+    numberContainer={"0": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "1": [10, 11, 12,13, 14, 15, 16, 17, 18, 19],,
+    "2": [20, 21, 22, 23,24, 25, 26, 27, 28, 29],
+    "3": [30, 31, 32, 33, 34, 35, 36, 37, 38, 39], 
+    "4": [40, 41, 42, 43, 44, 45, 46, 47, 48, 49], 
+    "5": [50, 51, 52, 53, 54, 55, 56, 57, 58, 59], 
+    "6": [60, 61, 62, 63, 64, 65, 66, 67, 68, 69], 
+    "7": [70, 71, 72, 73, 74, 75, 76, 77, 78, 79], 
+    "8": [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90],}
+    
+    # placing a number to particular position in the ticket 
+    counter = 0
+    while (counter < len(randomColList)):
+        colNum = randomColList[counter]
+        numbersListByIndex = numberContainer [str(colNum)] 
+        randomNumber = random.choice (numbersListByIndex)
+
+        if (randomNumber not in currentNumberList): 
+            numberBox = ticketGrid[row][colNum]
+            numberBox.configure (text=randomNumber, fg="black")
+            currentNumberList.append(randomNumber)
+            
+            counter+=1
+
     for row in range(0,3):
         random_col_list=[]
         counter=0
